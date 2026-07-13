ba7d5ac6239d6bacab11de0c609bae8738ca027c
Chocolate_Starfish <alex.beigel@gmail.com>
2026-07-13 16:47:47 +0300

    feat(expenses): separate scaffold file - all the logic will be handled within expenses

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:expenses_tracker/expenses.dart';
 
 void main() {
   runApp(
       MaterialApp(
           home: Scaffold(
               body: Center(
-                child: Text('Expenses'),
+                child: Expenses(),
               ),
           ),
       ),
