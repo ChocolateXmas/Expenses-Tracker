1e9895d4f2c28ba97d29f6d1008f34fd3f3cc11a
Chocolate_Starfish <alex.beigel@gmail.com>
2026-07-13 17:38:05 +0300

    feat(models-expense): added a category enum for a single expense, typo-safe category choice

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,12 +2,24 @@ import 'package:uuid/uuid.dart';
 
 var uuid = Uuid();
 
+enum Category {
+  food,
+  travel,
+  leisure,
+  work,
+}
+
 class Expense {
-  Expense({required this.title, required this.amount, required this.date})
-    : id = uuid.v5(Namespace.nil.value, title);
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v5(Namespace.nil.value, title);
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
